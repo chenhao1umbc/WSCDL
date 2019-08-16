604395541b12dd0c546078478fb805e4d129b26d
chenhao1umbc <chenhao1@umbc.edu>
2019-08-15 21:08:36 -0400

    working on Toepliz matrix

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 import time
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.linalg import toeplitz
+# from scipy.linalg import toeplitz   # This is too slow
 
 np.set_printoptions(linewidth=180)
 torch.set_printoptions(linewidth=180)
@@ -90,27 +90,68 @@ def fconv(an, bn):
     return cn
 
 
-def updateD0():
-    pass
+def toepliz(x, m=0):
+    """This is a the toepliz matrx for torch.tensor
+    input x has the shape of [N, T]
+        M is an interger
+    output tx has the shape of [N, M, T]
+    """
+    for i in range(x.shape[0]):
+        for ii in range(m):
+
+            pass
 
 
 def updateD(DD0SS0, X, Y, opts):
     """this function is to update the distinctive D using BPG-M, updating each d_k^(c)
-    input is initialed D,
-    sparse coeffecient S_bar
-        the structure is not a matrix for computation simplexity
-        S is 4-d tensor [N,C,T,K] [samples,classes,time series, num of atoms]
-        D is 3-d tensor [K,C,M] [num of atoms, classes, atom size]
+    input is initialed  DD0SS0
+    the data structure is not in matrix format for computation simplexity
+        S is 4-d tensor [N,C,K,T] [samples,classes, num of atoms, time series,]
+        D is 3-d tensor [C,K,M] [num of atoms, classes, atom size]
         S0 is 3-d tensor [N, K0, T]
         D0 is a matrix [K0, M]
-    training Data X
-    training labels Y \in Z ^(N*C)
-    training paremeters opts
-    output is updated D_bar
+        X is a matrix [N, T], training Data
+        Y is a matrix [N, C] \in {0,1}, training labels
+
+    'for better understanding the code please try:'
+    a = torch.rand(3, 4, 12)  # S0
+    b = torch.rand(4, 5)  # D0
+    k0, m = b.shape
+    s0 = a.unsqueeze(1)  # expand dimension for conv1d
+    d0 = b.flip(1).unsqueeze(0).unsqueeze(0)  # expand dimension for conv1d
+    for i in range(k0):
+        print(F.conv1d(s0[:, :, i, :], d0[:, :, i, :]))
+    'compare wth'
+    print(F.conv1d(a, b.flip(1).unsqueeze(1), groups=4))
     """
     D, D0, S, S0 = DD0SS0  # where DD0SS0 is a list
-    rn = conv(D0, S0)
+    N, K0, T = S0.shape
+    M =D0.shape[1]
+    M_2 = int(M/2)  # dictionary atom dimension
+    R = F.conv1d(S0, D0.flip(1).unsqueeze(1), groups=K0, padding=M-1).sum(1)[:, M_2:M_2+T]  # r is shape of [N, T)
+    C, K, _ = D.shape
+    D = D.flip(2).unsqueeze(2)  # D shape is [C,K,1, M]
+    DconvS = S[:, :, 0, :].clone()  # to avoid zeros for cuda decision, shape of [N, C, T]
+    Crange = torch.tensor(range(C))
+    for c in range(C):
+        DconvS[:, c, :] = F.conv1d(S[:, c, :, :], D[c, :, :, :], groups=K, padding=M-1).sum(1)[:, M_2:M_2+T]
+        # D*S, not the D'*S', And here D'*S' will not be updated for each d_k,c update
+        torch.cuda.empty_cache()
 
+    # '''update the current d_c,k '''
+    for c, k in [(i, j) for i in range(C) for j in range(K)]:
+        dck = D[c, k, :]  # shape of [M]
+        sck = S[:, c, k, :]  # shape of [N, T]
+        Tsck = toeplitz()
+        dck_conv_sck = F.conv1d(sck.unsqeeze(1), dck.reshape(1,1,M), padding=M-1).squeeze()[:, M_2:M_2+T]  # shape of [N,T]
+        c_prime = Crange[Crange != c]  # c_prime contains all the indexes
+        DpconvSp = ((1- Y[:, c_prime] - Y[:, c_prime]*Y[:, c].reshape(N, 1)).unsqueeze(2)*DconvS[:, c_prime, :]).sum(1)
+        b = (X - R - (DconvS.sum(1) - dck_conv_sck) - (DconvS[:, c, :] - dck_conv_sck) + DpconvSp)/2  # b is bn with all N
+        torch.cuda.empty_cache()
+
+
+def updateD0():
+    pass
 
 
 def load_data():
