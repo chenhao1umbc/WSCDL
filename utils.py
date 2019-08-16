10ef11a003a069eaa68af8a79ca097a1516baaf7
chenhao1umbc <chenhao1@umbc.edu>
2019-08-16 18:45:22 -0400

    finished bpg_m, acc_newton, and deleted numpy version of proj_d(acc_newton), bpg_m, and old functions e.g. fft_conv, conv

--- utils.py
+++ utils.py
@@ -15,79 +15,54 @@ torch.backends.cudnn.deterministic = True
 class OPT:
     """initial c the number of classes, k0 the size of shared dictionary atoms
     miu is the coeff of low-rank term, lamb is the coeff of sparsity """
-    def __init__(self, C=5, K0=10, K=20, miu=0.1, lamb=0.1,):
+    def __init__(self, C=5, K0=10, K=20, miu=0.1, lamb=0.1, delta = 0.9):
         self.C, self.K, self.K0 = C, K, K0
-        self.miu, self.lamb = miu, lamb
+        self.miu, self.lamb , self.delta = miu, lamb, delta
+        self.dev = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
-def bpg_m( x, M, Mw, gradf, type=0):
+def bpg_m(x_M_Minv_Mw_gradf, type=0):
     """typ1 =0 means updating dictionary itoms
-        type =1 meaning updating the sparse coeff. """
+        type =1 meaning updating the sparse coeff.
+        x_M_Minv_Mw_gradf is a list
+        """
+    x, M, Minv, Mw, gradf = x_M_Minv_Mw_gradf
     maxiter = 500
     if type == 0:
         d_old, d = x, x
         for i in range(500):
             d_til = d + Mw@(d -d_old)
-            nu = d_til - np.linalg.pinv(M)@gradf(d_til)
-            if norm(nu)**2 <=1 :
+            nu = d_til - Minv@gradf(d_til)
+            if torch.norm(nu).item()**2 <=1 :
                 d_new = nu
             else:
-                d_new = prox_d(M, -M@nu) # QCQP(P, q)
+                d_new = acc_newton(M, -M@nu)  # QCQP(P, q)
             d, d_old = d_new, d
-            if norm(d - d_old) < 1e-3:
+            if torch.norm(d - d_old).item() < 1e-3:
                 break
         return d
 
 
-def prox_d(P, q):
-    """proximal operator for majorized form using Accelorated Newton's method"""
+def acc_newton(P, q):
+    """proximal operator for majorized form using Accelorated Newton's method
+    follow the solutions of `convex optimization` by boyd, exercise 4.22, solving QCQP
+    where P is a square diagonal matrix, q is a vector
+    for update D  q =- M_D \nu = -MD@nu, P = M_D = MD
+    for update D0, q = -(M_D \nu + \rho z_k + Y_k), P = M_D + \rho I
+                    q = MD@nu + rho*Z + Y[:, k] , P = MD + rho*eye(M)
+    """
     psi = 0
     dim = P.shape[0]
     maxiter = 200
     for i in range(maxiter):
-        f_stroke = - 2 * np.sum(q * q * np.diag(P+psi*np.eye(dim))**(-3))
-        f = np.sum(q * q * np.diag(P+psi*np.eye(dim))**(-2))
-        psi_new = psi - 2 * f/f_stroke * (np.sqrt(f) - 1)
-        if psi_new - psi < 1e-5: # psi_new should always larger than psi
+        f_grad = - 2 * ((P.diag()+psi*torch.eye(dim))**(-3)*q*q).sum()
+        f = ((P.diag()+psi*torch.eye(dim))**(-2)*q*q).sum()
+        psi_new = psi - 2 * f/f_grad * (f.sqrt() - 1)
+        if psi_new.item() - psi.item() < 1e-5:  # psi_new should always larger than psi
             break
         else:
-            psi = psi_new
+            psi = psi_new.clone()
     return psi_new
-
-
-def conv(a, b):
-    """an, bn are in the shape (N, Ta), (N,Tb), where N is batch_size, T is sequence length,
-    an, and bn are all torch.tensor
-    """
-    pad = a.shape[1] if a.shape[1] < b.shape[1] else b.shape[1]
-    rab = torch.nn.functional.conv1d(a.unsqueeze(1), b.flip(1).unsqueeze(1), padding=pad-1)
-    return rab.squeeze()
-
-
-def fconv(an, bn):
-    """an, bn are numpy arrays, using fourier transform
-    an \in R^T*N, bn \in R^M*N, T>M,
-    using fft to do the 1-d convlution and trucate to the lenght T"""
-    ifft = np.fft.ifft
-    fft = np.fft.fft
-    zeros = np.zeros
-    l1, l2 = an.shape[0], bn.shape[0]
-    if l1 < l2:
-        l1, l2 = l2, l1 # make sure an is longer than bn
-    l = l1 + l2
-    try:
-        col = an.shape[1]
-    except IndexError:
-        col = 1
-    cn = zeros([l1, col])
-    start_point = l2 // 2
-    if col == 1:
-        an, bn = np.squeeze(an), np.squeeze(bn)
-        cn = ifft(fft(an, l) * fft(bn, l))[start_point:l1+start_point]
-    else:
-        for i in range(col):
-            cn[:, i] = ifft(fft(an[:, i], l) * fft(bn[:, i], l))[start_point:l1+start_point]
-    return cn
 
 
 def toeplitz(x, m=0):
@@ -98,7 +73,7 @@ def toeplitz(x, m=0):
     """
     dev = x.device
     N, T = x.shape
-    x_append0 = torch.cat([x, torch.zeros(N, m, device=dev)], dim=1)
+    x_append0 = torch.cat([x, torch.zeros(N, 2*m, device=dev)], dim=1)
     xm = x_append0.repeat(m, 1, 1).permute(1, 0, 2)  # shape of [N, m, T+2m]
     tx = torch.zeros(N, m, T, device=dev)
     m2 = int(m/2)
@@ -149,7 +124,14 @@ def updateD(DD0SS0, X, Y, opts):
         dck = D[c, k, :]  # shape of [M]
         sck = S[:, c, k, :]  # shape of [N, T]
         Tsck = toeplitz(sck)  # shape of [N, M, T]
-        dck_conv_sck = F.conv1d(sck.unsqeeze(1), dck.reshape(1,1,M), padding=M-1).squeeze()[:, M_2:M_2+T]  # shape of [N,T]
+        abs_Tsck = abs(Tsck)
+        Mw = opts.delta * torch.eye(M, device=opts.dev)
+        MD_diag = ((abs_Tsck @ abs_Tsck).sum(0) @ torch.ones(M, 1, device=opts.dev)).squeeze()
+        MD = MD_diag.diag()
+        MD_inu = (1/MD_diag).diag()
+        nu = 0
+
+        dck_conv_sck = F.conv1d(sck.unsqeeze(1), dck.reshape(1, 1, M), padding=M-1).squeeze()[:, M_2:M_2+T]  # shape of [N,T]
         c_prime = Crange[Crange != c]  # c_prime contains all the indexes
         DpconvSp = ((1- Y[:, c_prime] - Y[:, c_prime]*Y[:, c].reshape(N, 1)).unsqueeze(2)*DconvS[:, c_prime, :]).sum(1)
         b = (X - R - (DconvS.sum(1) - dck_conv_sck) - (DconvS[:, c, :] - dck_conv_sck) + DpconvSp)/2  # b is shape of [N, T]
