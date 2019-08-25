a86db510f0eb3e6f2862e2caf3a0a032c2247e42
Chen Hao <chenhao1@umbc.edu>
2019-08-24 23:54:00 -0400

    adding synthetic data still working on it

--- utils.py
+++ utils.py
@@ -584,7 +584,7 @@ def load_data(opts):
     """
     '''The common features'''
     x = torch.arange(30).float()
-    c_f = torch.sin(x*2*np.pi/30)
+    feature0 = torch.sin(x*2*np.pi/30)
     feature1 = torch.sin(x * 2 * np.pi / 15) + torch.sin(x * 2 * np.pi / 10)
     feature2 = torch.sin(x * 2 * np.pi / 20) + torch.sin(x * 2 * np.pi / 5) + torch.sin(x * 2 * np.pi / 8)
     feature3 = torch.zeros(30)
@@ -592,8 +592,9 @@ def load_data(opts):
     feature3 = feature3 + torch.sin(x * 2 * np.pi / 13)
     feature4 = torch.zeros(30)
     feature4[np.r_[np.arange(10), np.arange(20, 30)]] = 1
-    feature4 = feature4 + torch.sin(x *  np.pi / 6)
-
+    feature4 = feature4 + torch.sin(x * np.pi / 6)
+    start_point = torch.randint(0, 21, (1,))
+    idx_feat = torch.randint(0, 2, (1,))
 
     X, Y = 0, 0
 
