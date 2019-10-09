cecaa6ca36772a6103be7166079e2fdeaba8125b
chenhao1umbc <chenhao1@umbc.edu>
2019-10-09 12:05:28 -0400

    adding readwave function; auto tuning toy

--- beta.py
+++ beta.py
@@ -8,14 +8,15 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "1, 3"
 opts = OPT()
 opts.snr = 200
 opts.lamb = 2.7 # for sparsity penalty
-opts.eta = 4.1 # for label penalty
+opts.eta = 0 # for label penalty
 opts.mu = 0  # for low rank penalty
 opts.show_details = False  # default as true
 acc_av = 0.0
 
 # training section
 X, Y, ft = load_toy(opts)
-for opts.eta in [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 3.85, 4.1]:
+for opts.lamb in range(20, 31):
+    opts.lamb= opts.lamb/10.0
     for f in range(5):
         D, D0, S, S0, W = init(X, opts)
         D, D0, S, S0, W, loss = train(D, D0, S, S0, W, X, Y, opts)
@@ -27,6 +28,6 @@ for opts.eta in [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 3.85, 4.1]:
         acc, y_hat, S_t, S0_t = test(D, D0, S_t, S0_t, W, X_test, Y_test, opts)
         acc_av = acc_av + acc
     print('\nThe test data accuracy is : ', acc_av/5)
+    torch.save([acc_av/5, opts.lamb], '../acc_av' + tt().strftime("%y%m%d_%H_%M_%S") + '.pt')
     acc_av = 0
-    torch.save([acc_av, opts.eta], '../acc_av' + tt().strftime("%y%m%d_%H_%M_%S") + '.pt')
 print('done')
--- utils.py
+++ utils.py
@@ -4,10 +4,12 @@ import os
 import pickle
 import time
 import datetime
+import wave
 import torch
 import torch.nn.functional as F
 import numpy as np
 import matplotlib.pyplot as plt
+
 
 tt = datetime.datetime.now
 # torch.set_default_dtype(torch.double)
@@ -1662,3 +1664,44 @@ def awgn(x, snr):
     noise = torch.tensor(np.sqrt(variance) * np.random.normal(0, 1, x.shape), device=x.device)
 
     return x+noise.to(x.dtype)
+
+
+def _wav2array(nchannels, sampwidth, data):
+    """data must be the string containing the bytes from the wav file."""
+    num_samples, remainder = divmod(len(data), sampwidth * nchannels)
+    if remainder > 0:
+        raise ValueError('The length of data is not a multiple of '
+                         'sampwidth * num_channels.')
+    if sampwidth > 4:
+        raise ValueError("sampwidth must not be greater than 4.")
+
+    if sampwidth == 3:
+        a = np.empty((num_samples, nchannels, 4), dtype=np.uint8)
+        raw_bytes = np.fromstring(data, dtype=np.uint8)
+        a[:, :, :sampwidth] = raw_bytes.reshape(-1, nchannels, sampwidth)
+        a[:, :, sampwidth:] = (a[:, :, sampwidth - 1:sampwidth] >> 7) * 255
+        result = a.view('<i4').reshape(a.shape[:-1])
+    else:
+        # 8 bit samples are stored as unsigned ints; others as signed ints.
+        dt_char = 'u' if sampwidth == 1 else 'i'
+        a = np.fromstring(data, dtype='<%s%d' % (dt_char, sampwidth))
+        result = a.reshape(-1, nchannels)
+    return result
+
+
+def readwav(file):
+    """
+    Read a wav file.
+    Returns the frame rate, sample width (in bytes) and a numpy array
+    containing the data.
+    This function does not read compressed wav files.
+    """
+    wav = wave.open(file)
+    rate = wav.getframerate()
+    nchannels = wav.getnchannels()
+    sampwidth = wav.getsampwidth()
+    nframes = wav.getnframes()
+    data = wav.readframes(nframes)
+    wav.close()
+    array = _wav2array(nchannels, sampwidth, data)
+    return rate, sampwidth, array
