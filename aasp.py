9086106a54e30c9f4273e95151fd9796262c940a
chenhao1umbc <chenhao1@umbc.edu>
2019-10-17 09:52:31 -0400

    adding terminal version of aasp

--- aasp.py
+++ aasp.py
@@ -4,7 +4,7 @@ The default data type is torch.tensor with precision float32
 
 from utils import *
 os.environ["CUDA_VISIBLE_DEVICES"] = "3"
-opts = OPT(C=16, K0=3, K=3, M=30)
+opts = OPT(C=16, K0=3, K=3, M=80)
 opts.lamb = 0.1 # for sparsity penalty
 opts.eta = 0 # for label penalty
 opts.mu = 0  # for low rank penalty
--- aasp_terminal.py
+++ aasp_terminal.py
@@ -11,3 +11,8 @@ X, Y = load_data(opts)
 D, D0, S, S0, W = init(X, opts)
 D, D0, S, S0, W, loss = train(D, D0, S, S0, W, X, Y, opts)
 if opts.save_results: save_results(D, D0, S, S0, W, opts, loss)
+
+X_test, Y_test, _ = load_data(opts, data='val')
+_, _, S_t, S0_t, _ = init(X_test, opts)
+acc, y_hat, S_t, S0_t = test(D, D0, S_t, S0_t, W, X_test, Y_test, opts)
+print('\nThe test data accuracy is : ', acc)
