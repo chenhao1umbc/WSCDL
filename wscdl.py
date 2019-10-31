592a4c6998ff6784e383eb10ae38ece96b78c416
chenhao1umbc <chenhao1@umbc.edu>
2019-10-31 12:36:42 -0400

    add toy to beta to understand why training data as the test data may not have good result

--- wscdl.py
+++ wscdl.py
@@ -3,9 +3,9 @@ The default data type is torch.tensor with precision float32
 """
 
 from utils import *
-os.environ["CUDA_VISIBLE_DEVICES"] = "1, 3"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 opts = OPT()
-opts.snr = 0
+opts.snr = 10
 opts.lamb = 2.7 # for sparsity penalty
 opts.eta = 3 # for label penalty
 opts.mu = 0  # for low rank penalty
@@ -20,7 +20,7 @@ plot_result(X, Y, D, D0, S, S0, W, opts.ft, loss, opts)
 # D, D0, S, S0, W, opts, loss = torch.load('DD0SS0Woptsloss.pt')
 
 # testing section
-X_test, Y_test, _ = load_toy(opts, test='train')
+X_test, Y_test, _ = load_toy(opts, test='cv')
 _, _, S_t, S0_t, _ = init(X_test, opts)
 acc, y_hat, S_t, S0_t, loss_t = test(D, D0, S_t, S0_t, W, X_test, Y_test, opts)
 print('\nThe test data accuracy is : ', acc.acc)
