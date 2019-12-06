81a9cf571f832e8eefd7d32a92bef0874f1b8f19
chenhao1 <chenhao1@umbc.edu>
2019-12-06 13:22:20 -0500

    adding the result of different window size

--- others_code/main.m
+++ others_code/main.m
@@ -27,10 +27,13 @@ addpath('WSCADL-YOU_master')
 
 %% run aasp
 run settings_aasp
-acc = zeros(5, 1); 
-rec = zeros(5, 1); 
-prec = zeros(5, 1); 
+runs = 3;
+acc = zeros(runs, 1); 
+rec = zeros(runs, 1); 
+prec = zeros(runs, 1); 
+l = [0.1 0.01 0.001];
 for i=1:runs
+    lamb = l(i);
     permidx(i,:)=randperm(No_spect);
     trainY = Y(permidx(i,1:no_train),:);
     trainX = X(:,:,permidx(i,1:no_train));
@@ -60,9 +63,47 @@ for i=1:runs
     y_hat_test = get_signal_label(w{i}, X_test, option);  % newly written function get the predicted signal labels
     test_acc = sum((y_hat_test - Y_test) == 0, 'all')/numel(y_hat_test)
     [test_recall, test_prec] = prec_rec(y_hat_test, Y_test)
-
 end
 
+
+winsize=3;
+no_para=F*winsize+1
+acc = zeros(runs, 1); 
+rec = zeros(runs, 1); 
+prec = zeros(runs, 1); 
+l = [0.1 0.01 0.001];
+for i=1:runs
+    lamb = l(i);
+    permidx(i,:)=randperm(No_spect);
+    trainY = Y(permidx(i,1:no_train),:);
+    trainX = X(:,:,permidx(i,1:no_train));
+    trainNvec = N*ones(1,no_train);%N_vec(permidx(i,1:no_train));
+    wini=1e-3*randn(no_para,C,K);
+    
+    option.dsiter=100;% display for every 1000 iteration;
+    [ w{i},~,rllharr{i},garr{i}] = EMPosteriorRegularized_batch(wini,trainX,trainY,trainNvec,EMiterations,Miterations,0,gamma,option,lamb);
+    
+    % this part is newly added to see the signal level accuracy result
+    valX = X(:,:,permidx(i,no_train+1:end));
+    valY = Y(permidx(i,no_train+1:end),:);
+    wtx = wtimesx(w{i},valX,option);  % this function was originally defined in EMPosteriorRegularized_batch.m file
+    y_hat = get_signal_label(w{i}, valX, option);  % newly written function get the predicted signal labels
+    acc(i) = sum((y_hat - valY) == 0, 'all')/numel(y_hat)
+    [rec(i), prec(i)] = prec_rec(y_hat, valY)
+
+    % this part is for the test data
+    x = importdata('x_test_80_50.txt');
+    x = x./sqrt(sum(x.*x, 2));  % normalize data
+    X_test = zeros(80, 50, size(x, 1));
+    for ii = 1:size(x, 1)
+        X_test(:,:, ii) = reshape(x(ii,:), 50, 80)';
+    end
+    Y_test = importdata('y_test_80_50.txt');
+    wtx = wtimesx(w{i},X_test,option);  % this function was originally defined in EMPosteriorRegularized_batch.m file
+    y_hat_test = get_signal_label(w{i}, X_test, option);  % newly written function get the predicted signal labels
+    test_acc = sum((y_hat_test - Y_test) == 0, 'all')/numel(y_hat_test)
+    [test_recall, test_prec] = prec_rec(y_hat_test, Y_test)
+end
 % [~, ind] = max(acc);  % find the best W
 % x = importdata('x_test_80_50.txt');
 % x = x./sqrt(sum(x.*x, 2));  % normalize data
