2c2e5a66817a0b9617cc20747512d5d0de5e06a6
Chen Hao <chenhao1@umbc.edu>
2020-05-02 12:53:10 -0400

    fixed the bug in calculating low-rank regularizer in loss function

--- utils2.py
+++ utils2.py
@@ -267,7 +267,7 @@ def loss_fun_special(X, Y, D, D0, S, S0, W, opts):
     Dr = D.reshape(CK, 1, Dh, Dw)
     for ck in range(CK):
         DconvS0[:, ck] = Func.conv1d(S.reshape(N,CK,1,T)[:,ck],Dr[ck].permute(1,0,2).flip(2),padding=opts.offset).squeeze()
-    DconvS = DconvS0.reshape(N, C, K, F, T)   # shape of [N,C,K,F,T]
+    DconvS = DconvS0.reshape(N, CK, F, T)   # shape of [N,CK,F,T]
 
     ycDcconvSc = (Y.reshape(NC, 1) * DconvS.reshape(NC, -1)).reshape(N, CK, F, T).sum(1)  # output shape of (N, F, T)
     ycpDcconvSc = ((1 - Y).reshape(NC, 1) * DconvS.reshape(NC, -1)).reshape(N, CK, F, T).sum(1)  # output shape of (N, F, T)
