43b336c974dc9741d079b06107a6fa416834d53a
chen_hao <chenhao1@umbc.edu>
2020-06-06 23:04:02 -0400

    small changes in main_2d.py

--- main_2d.py
+++ main_2d.py
@@ -5,13 +5,13 @@ The default data type is torch.tensor with precision float32
 from utils2 import *
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 opts = OPT(C=16, K0=1, K=3)
-opts.init, opts.shuffle, opts.show_details = 'good', False, False
+opts.init, opts.shuffle, opts.show_details = 'good', False, True
 opts.Dh, opts.Dw, opts.batch_size = 256, 15, 3
 opts.lamb, opts.eta, opts.mu = 1, 0.1, 0.01 # for sparsity penalty, label penalty, low rank penalty
 
 # training section
 X, Y = load_data(opts, data='train')
-D, D0, S, S0, W, loss = train(X[:5], Y[:5], opts)
+D, D0, S, S0, W, loss = train(X, Y, opts)
 if opts.save_results: save_results(D, D0, S, S0, W, opts, loss)
 plot_result(X, Y, D, D0, S, S0, W, ft=0, loss=loss, opts=opts)
 # D, D0, S, S0, W, opts, loss = torch.load('DD0SS0Woptsloss.pt')
