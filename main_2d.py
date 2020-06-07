12997176b17d3a9a20d90744403893403a150c2a
chen_hao <chenhao1@umbc.edu>
2020-06-06 23:06:42 -0400

    small changes in main_2d.py

--- main_2d.py
+++ main_2d.py
@@ -6,7 +6,7 @@ from utils2 import *
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 opts = OPT(C=16, K0=1, K=3)
 opts.init, opts.shuffle, opts.show_details = 'good', False, True
-opts.Dh, opts.Dw, opts.batch_size = 256, 15, 3
+opts.Dh, opts.Dw, opts.batch_size = 256, 15, -1
 opts.lamb, opts.eta, opts.mu = 1, 0.1, 0.01 # for sparsity penalty, label penalty, low rank penalty
 
 # training section
