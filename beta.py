bb6bf6ceff1d2584a08a50f3728448615685ba3a
chenhao1umbc <chenhao1@umbc.edu>
2021-02-24 16:40:17 -0500

    input more details in saved tunning result

--- beta.py
+++ beta.py
@@ -21,8 +21,9 @@ X, Y, yy = load_data(opts, data='train') # shape of [n_sample, f, t]
 X_val, Y_val, yy_val = load_data(opts, data='val')
 
 #%% training section
+res = []
 for Dw in [15, 21, 25, 29, 35]:
-    for lamb in [0.1, 0.01, 0.001]:
+    for lamb in [0.1, 0.01, 0.001, 0.5]:
         for lamb_ratio in [1, 0.5, 0.1 ]:
             for eta in [0.1, 0.01, 1]:
                 for mu in [0.1, 0.01, 1]:
@@ -30,13 +31,15 @@ for Dw in [15, 21, 25, 29, 35]:
                                         Dw, lamb, lamb_ratio*lamb, eta, mu 
                     D, D0, S, S0, W, loss = train(X, Y, opts)
                     save_results(D, D0, S, S0, W, opts, loss)
-                    
+
                     _, _, St, S0t, _ = init(X_val, opts)
                     acc, y_hat, St, S0t, loss_t = test(D, D0, St, S0t, W, X_val, Y_val, opts)
                     print('The validation accuracy, recall and precision are : ',\
                              acc.acc, acc.recall, acc.f1)
-
-
+                    print('with parameters as : ', opts.Dw, opts.lamb, opts.lamb0, opts.eta, opts.mu)
+                    print('\n')
+                    res.append(((acc.acc, acc.recall, acc.f1),(opts.Dw, opts.lamb, opts.lamb0, opts.eta, opts.mu)))
+                    torch.save(res, 'tunning.pt')
 #%% just validation section, supposed that training is done
 D, D0, S, S0, W, opts, loss = torch.load(
     '../saved_dicts/[3, 1, 5, 0.1, 0.01, 1, 0.1]DD0SS0Woptsloss.pt')
