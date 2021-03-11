d1cb8e8b52e30467973ea07ba435228923a6f97d
chenhao1umbc <chenhao1@umbc.edu>
2021-03-10 19:22:29 -0500

    vgg16 give some init results

--- deepmiml_repr.py
+++ deepmiml_repr.py
@@ -2,43 +2,54 @@
 This code is based on DeepMIML.demo.demo_train_miml_vgg.py
 """
 #%% load package
+import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dropout
-from tensorflow. keras.layers import Convolution2D
-import sys
-sys.path.insert(0, "lib")
+from tensorflow.keras.layers import Flatten, Dense, Dropout
+from tensorflow.keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D
+
+import scipy.io as sio
+import sklearn.metrics as metrics
 
 # from DeepMIML.lib.cocodemo import COCODataset, COCODataLayer
 from Deep_MIML_repr.deepmiml import DeepMIML
 from DeepMIML.lib.deepmiml.utils import save_keras_model
 from Deep_MIML_repr.vgg_16 import VGG_16
 
+"limit the gpu memory usage"
+gpu = tf.config.experimental.list_physical_devices('GPU')
+for i in range(len(gpu)):
+    tf.config.experimental.set_memory_growth(gpu[i], True)
+# tf.keras.applications.VGG16() # to load the pre-trained weights, for first use
+
 #%%
 loss = "binary_crossentropy"
-nb_epoch = 10
+nb_epoch = 100
 batch_size = 32
-L = 80
-K = 20
+L = 10
+K = 5
 model_name = "miml_vgg_16"
 
-vgg_model_path = "/home/chenhao1/.keras/models/vgg16_weights_tf_dim_ordering_tf_kernels.h5"
+vgg_model_path = "../data/vgg16_weights_tf_dim_ordering_tf_kernels.h5"
 base_model = VGG_16(vgg_model_path)
-base_model = Sequential(layers=base_model.layers[: -7])
-base_model.add(Convolution2D(512, 1, activation="relu"))
-base_model.add(Dropout(0.5))
+model = Sequential(layers=ZeroPadding2D((1,1),input_shape=(100, 500, 1)))
+model.add(Convolution2D(3, 1, activation="relu"))
+model.add(Sequential(base_model.layers[1: -7]))
+model.add(Convolution2D(512, 1, activation="relu"))
+model.add(Dropout(0.5))
 
-deepmiml = DeepMIML(L=L, K=K, base_model=base_model)
-# deepmiml.model.summary()
+deepmiml = DeepMIML(L=L, K=K, base_model=model)
+deepmiml.model.summary()
+
+"load training data"
+mat = sio.loadmat('../data/ESC10/esc10_tr.mat')
+x, y, yy = mat['X'], mat['Y'], mat['yy']
 
 #%%
 print("Compiling Deep MIML Model...")
 deepmiml.model.compile(optimizer="adadelta", loss=loss, metrics=["accuracy"])
 
 print("Start Training...")
-samples_per_epoch = data_layer.num_images
-deepmiml.model.fit_generator(data_layer.generate(),
-        samples_per_epoch=samples_per_epoch,
-        nb_epoch=nb_epoch)
+deepmiml.model.fit(x=x[...,None], y=y, epochs=nb_epoch, validation_split=0.25)
 
 # save_keras_model(deepmiml.model, "outputs/{}/{}".format(dataset.name, model_name))
 
@@ -57,10 +68,21 @@ num_images = dataset.num_images
 y_pred = np.zeros((num_images, dataset.num_classes))
 y_gt = np.zeros((num_images, dataset.num_classes))
 for i in range(0, num_images, batch_size):
-        if i // batch_size % 10 == 0:
-                print("[progress] ({}/{})".format(i, num_images))
-        x_val_mini, y_val_mini = data_layer.get_data(i, i + batch_size)
-        y_pred_mini = model.predict(x_val_mini)
-        y_pred[i: i + batch_size] = y_pred_mini
-        y_gt[i: i + batch_size] = y_val_mini
-(y_gt, y_pred, threshold_value=0.5)
+    if i // batch_size % 10 == 0:
+        print("[progress] ({}/{})".format(i, num_images))
+    x_val_mini, y_val_mini = data_layer.get_data(i, i + batch_size)
+    y_pred_mini = model.predict(x_val_mini)
+    y_pred[i: i + batch_size] = y_pred_mini
+    y_gt[i: i + batch_size] = y_val_mini
+    metrics.f1_score(y_gt, y_pred, threshold_value=0.5)
+
+
+#%%
+with open('/home/chenhao1/Matlab/WSCDL/you_raich.txt', 'r') as f:
+    res = f.readlines()
+
+count = 0
+for i, s in enumerate(res):
+    if s == 'rec =\n':
+        count += 1
+        print(res[i+2])
