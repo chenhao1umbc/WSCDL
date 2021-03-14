08aca200823a59f9843df556878aa425a1b631e7
chenhao1umbc <chenhao1@umbc.edu>
2021-03-13 22:37:54 -0500

    make deepmiml run without errors

--- deepmiml_repr.py
+++ deepmiml_repr.py
@@ -4,15 +4,16 @@ This code is based on DeepMIML.demo.demo_train_miml_vgg.py
 #%% load package
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Flatten, Dense, Dropout
+from tensorflow.keras.layers import Flatten, Dense, Dropout, Reshape
 from tensorflow.keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D
 
 import scipy.io as sio
 import sklearn.metrics as metrics
+import matplotlib.pyplot as plt
 
 # from DeepMIML.lib.cocodemo import COCODataset, COCODataLayer
 from Deep_MIML_repr.deepmiml import DeepMIML
-from DeepMIML.lib.deepmiml.utils import save_keras_model
+# from DeepMIML.lib.deepmiml.utils import save_keras_model
 from Deep_MIML_repr.vgg_16 import VGG_16
 
 "limit the gpu memory usage"
@@ -24,17 +25,18 @@ for i in range(len(gpu)):
 #%%
 loss = "binary_crossentropy"
 nb_epoch = 100
-batch_size = 32
+batch_size = 64
 L = 10
 K = 5
 model_name = "miml_vgg_16"
 
-vgg_model_path = "../data/vgg16_weights_tf_dim_ordering_tf_kernels.h5"
-base_model = VGG_16(vgg_model_path)
+# vgg_model_path = "../data/vgg16_weights_tf_dim_ordering_tf_kernels.h5"
+# base_model = VGG_16(vgg_model_path)
+# original model is too large easily overfits, validation loss increase but train loss descrease
 model = Sequential(layers=ZeroPadding2D((1,1),input_shape=(100, 500, 1)))
-model.add(Convolution2D(3, 1, activation="relu"))
-model.add(Sequential(base_model.layers[1: -32])) # originally -7 in the paper, but model too large
-model.add(Convolution2D(512, 1, activation="relu"))
+model.add(Convolution2D(4, 3, activation='relu'))
+model.add(Convolution2D(4, 3, activation='relu'))
+model.add(MaxPooling2D((5,10), strides=(4,5)))
 model.add(Dropout(0.5))
 
 deepmiml = DeepMIML(L=L, K=K, base_model=model)
@@ -46,35 +48,44 @@ x, y, yy = mat['X'], mat['Y'], mat['yy']
 
 #%%
 print("Compiling Deep MIML Model...")
-deepmiml.model.compile(optimizer="adadelta", loss=loss, metrics=["accuracy"])
+deepmiml.model.compile(optimizer="adam", loss=loss, metrics=["accuracy"])
 
 print("Start Training...")
-deepmiml.model.fit(x=x[...,None], y=y, epochs=nb_epoch, validation_split=0.25)
+history = deepmiml.model.fit(x=x[...,None], y=y, \
+    batch_size = batch_size, epochs=nb_epoch, validation_split=0.25)
+# list all data in history
+print(history.history.keys())
+# summarize history for accuracy
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
 
 # save_keras_model(deepmiml.model, "outputs/{}/{}".format(dataset.name, model_name))
 
 
 #%% test part
 model = deepmiml.model
-print("Compiling Deep MIML Model...")
-model.compile(optimizer="adadelta", loss="binary_crossentropy")
-
-# # crate data layer
-# dataset = COCODataset("data/coco", "val", "2014")
-# data_layer = COCODataLayer(dataset, batch_size=batch_size)
+mat = sio.loadmat('../data/ESC10/esc10_tr.mat')
+xt, yt, yyt = mat['X'], mat['Y'], mat['yy']
 
 print("Start Predicting...")
-num_images = dataset.num_images
-y_pred = np.zeros((num_images, dataset.num_classes))
-y_gt = np.zeros((num_images, dataset.num_classes))
-for i in range(0, num_images, batch_size):
-    if i // batch_size % 10 == 0:
-        print("[progress] ({}/{})".format(i, num_images))
-    x_val_mini, y_val_mini = data_layer.get_data(i, i + batch_size)
-    y_pred_mini = model.predict(x_val_mini)
-    y_pred[i: i + batch_size] = y_pred_mini
-    y_gt[i: i + batch_size] = y_val_mini
-    metrics.f1_score(y_gt, y_pred, threshold_value=0.5)
+y_pred = model.predict(xt[...,None])
+thr = 0.23
+y_pred[y_pred>=thr] = 1
+y_pred[y_pred<thr] = 0
+metrics.f1_score(yt.flatten(), y_pred.flatten())
 
 
 #%%
