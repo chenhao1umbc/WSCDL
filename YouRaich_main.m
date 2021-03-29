2954aa5b46d6c03fc3a626ae76bee12054201fec
chenhao1umbc <chenhao1@umbc.edu>
2021-03-29 00:02:45 -0400

    fixed an big error in you_raich code, I need to retune the parameter from beginning

--- YouRaich_main.m
+++ YouRaich_main.m
@@ -23,7 +23,7 @@ rng(0)
 %     valX = X(:,:,permidx(i,no_train+1:end));
 %     valY = Y(permidx(i,no_train+1:end),:);
 %     wtx = wtimesx(w{i},valX,option);  % this function was originally defined in EMPosteriorRegularized_batch.m file
-%     y_hat = get_signal_label(w{i}, valX, option);  % newly written function get the predicted signal labels
+%     [y_hat, p] = get_signal_label(w{i}, valX, option);  % newly written function get the predicted signal labels
 %     acc(i) = sum((y_hat - valY) == 0, 'all')/numel(y_hat)
 % end
 
@@ -37,7 +37,7 @@ snr=10000;
 opt.method='batch';%'online';
 opt.priorType='conv';%'times';
 opt.estepType1='chain';%'tree' for e-step;
-opt.addone=0;%add bias term;
+opt.addone=1;%add bias term;
 opt.conv=1;%or'fft' for convolution method;
 opt.display=0;%display the words and probablities; 
 opt.dsiter= 10; % display for every 1000 iteration;
@@ -52,7 +52,7 @@ K=2;
 gamma=0;
 runs=1;
 perc=0.75;  % perc * n_samples for training, default as 0.75
-EMiterations = 200;
+EMiterations = 1; 
 Miterations=1;
 no_train=ceil(perc*No_spect);
 no_test=No_spect-no_train;
@@ -66,89 +66,20 @@ py.importlib.reload(myfunc);
 opt.myfunc = myfunc; % added the myfunc to option 
 
 
-% %% rough tunning
-% lamb_pool=[1, 0.1, 0.01, 1e-3, 1e-4];
-% winsize_pool=[30, 50, 100, 150];
-% N_pool=[5, 10, 20, 50, 100, 200];%sparsity constraints;
-% 
-% acc = zeros(runs, 1); 
-% rec = zeros(runs, 1); 
-% prec = zeros(runs, 1); 
-% 
-% for i=1:runs
-%     permidx=randperm(No_spect);
-%     trainY = Y(permidx(1:no_train),:);
-%     trainX = X(:,:,permidx(1:no_train));
-%     
-%     valX = X(:,:,permidx(no_train+1:end));
-%     valY = Y(permidx(no_train+1:end),:);
-% 
-%     x = permute(trainX, [3, 1, 2]);
-%     px = py.torch.tensor(py.numpy.array(x));
-%     px = px.cuda().float();
-%     opt.px = px; % added the myfunc to option
-%     
-%     for lamb = lamb_pool
-%         for winsize = winsize_pool
-%             for N = N_pool
-% lamb
-% winsize
-% N
-% opt.winsize = winsize;
-% opt.px = px; % opt.px will be changed in validation
-% trainNvec = N*ones(1,no_train);%N_vec(permidx(i,1:no_train));
-% if opt.addone
-%     no_para=F*winsize+1;
-% else
-%     no_para=F*winsize;
-% end
-% wini=1e-3*randn(no_para,C,K);
-% [ w,~,loss,garr] = EMPosteriorRegularized_batch(...
-%     wini,trainX,trainY,trainNvec,EMiterations,Miterations,0,gamma,opt,lamb);
-% 
-% % this part is newly added to see the signal level accuracy result
-% y_hat = get_signal_label(w, valX, opt);  % opt.px will be changed
-% acc = sum((y_hat - valY) == 0, 'all')/numel(y_hat)
-% [rec, prec] = prec_rec(valY, y_hat)
-% f1 = 2/(1/(rec+1e-30) + 1/(prec+1e-30))
-% 
-%             end % end of N loop
-%         end % end of winsize loop
-%     end % end of lamb
-% end % end of runs
-% 
-% % # best lamb, winzize, N, K=1
-% % # 10, 30, 200
-% % # 10, 100, 10
-% % # 10, 50, 50
-% 
-% % # best lamb, winzize, N, k=2
-% % # 1, 150, 200
-% % # 0.01, 50, 50
-% % # 0.1, 100, 50
+%% rough tunning
+lamb_pool=[10, 1, 0.1, 0.01, 1e-3, 1e-4];
+winsize_pool=[30, 50, 100, 150];
+N_pool=[5, 10, 20, 50, 100, 200];%sparsity constraints;
 
-%% finer tunning
-
-runs = 5;
-for i = 1:6
-if i == 1 [lamb, winzize, N, K] = deal(10, 30, 200, 1); end
-if i == 2 [lamb, winzize, N, K] = deal(10, 100, 10, 1); end
-if i == 3 [lamb, winzize, N, K] = deal(10, 50, 50, 1); end
-if i == 4 [lamb, winzize, N, K] = deal(1, 150, 200, 2); end
-if i == 5 [lamb, winzize, N, K] = deal(0.01, 50, 50, 2); end
-if i == 6 [lamb, winzize, N, K] = deal(0.1, 100, 50, 2); end
 acc = zeros(runs, 1); 
-f1 = zeros(runs, 1); 
-opt.C = C;
-opt.F = F;
-opt.T = T;
-opt.K = K;
+rec = zeros(runs, 1); 
+prec = zeros(runs, 1); 
 
-    for r=1:runs
+for i=1:runs
     permidx=randperm(No_spect);
     trainY = Y(permidx(1:no_train),:);
     trainX = X(:,:,permidx(1:no_train));
-
+    
     valX = X(:,:,permidx(no_train+1:end));
     valY = Y(permidx(no_train+1:end),:);
 
@@ -156,34 +87,150 @@ opt.K = K;
     px = py.torch.tensor(py.numpy.array(x));
     px = px.cuda().float();
     opt.px = px; % added the myfunc to option
+    
+    for lamb = lamb_pool
+        for winsize = winsize_pool
+            for N = N_pool
+                
+opt.px = px; % opt.px will be changed in validation, do not delete               
+lamb
+winsize
+N
+opt.winsize = winsize;
+trainNvec = N*ones(1,no_train);%N_vec(permidx(i,1:no_train));
+if opt.addone
+    no_para=F*winsize+1;
+else
+    no_para=F*winsize;
+end
+wini=1e-3*randn(no_para,C,K);
+[ w,~,loss,garr] = EMPosteriorRegularized_batch(...
+    wini,trainX,trainY,trainNvec,EMiterations,Miterations,0,gamma,opt,lamb);
 
-    opt.winsize = winsize;
-    opt.px = px; % opt.px will be changed in validation
-    trainNvec = N*ones(1,no_train);%N_vec(permidx(i,1:no_train));
-    if opt.addone
-        no_para=F*winsize+1;
-    else
-        no_para=F*winsize;
-    end
-    wini=1e-3*randn(no_para,C,K);
-    [ w,~,loss,garr] = EMPosteriorRegularized_batch(...
-        wini,trainX,trainY,trainNvec,EMiterations,Miterations,0,gamma,opt,lamb);
+% this part is newly added to see the signal level accuracy result
+[y_hat, p] = get_signal_label(w, valX, opt);  % opt.px will be changed
+acc = sum((y_hat - valY) == 0, 'all')/numel(y_hat)
+[rec, prec] = prec_rec(valY, y_hat)
+f1 = 2/(1/(rec+1e-30) + 1/(prec+1e-30))
 
-    % this part is newly added to see the signal level accuracy result
-    y_hat = get_signal_label(w, valX, opt);  % opt.px will be changed
-    acc = sum((y_hat - valY) == 0, 'all')/numel(y_hat)
-    [rec, prec] = prec_rec(valY, y_hat)
-    f1(r) = 2/(1/(rec+1e-30) + 1/(prec+1e-30))
+            end % end of N loop
+        end % end of winsize loop
+    end % end of lamb
+end % end of runs
 
-    end % end of runs
-    i
-    mean_f1 = mean(f1)
-end % end of i
+% # best lamb, winzize, N, K=1
+% # 10, 30, 200
+% # 10, 100, 10
+% # 10, 50, 50
 
-% i = 6 is the best
-% lamb, winzize, N, K
-% 0.1, 100, 50, 2
+% # best lamb, winzize, N, k=2
+% # 1, 150, 200
+% # 0.01, 50, 50
+% # 0.1, 100, 50
+
+%% finer tunning
+% 
+% runs = 5;
+% for i = 1:6
+% if i == 1 [lamb, winzize, N, K] = deal(10, 30, 200, 1); end
+% if i == 2 [lamb, winzize, N, K] = deal(10, 100, 10, 1); end
+% if i == 3 [lamb, winzize, N, K] = deal(10, 50, 50, 1); end
+% if i == 4 [lamb, winzize, N, K] = deal(1, 150, 200, 2); end
+% if i == 5 [lamb, winzize, N, K] = deal(0.01, 50, 50, 2); end
+% if i == 6 [lamb, winzize, N, K] = deal(0.1, 100, 50, 2); end
+% acc = zeros(runs, 1); 
+% f1 = zeros(runs, 1); 
+% opt.C = C;
+% opt.F = F;
+% opt.T = T;
+% opt.K = K;
+% 
+%     for r=1:runs
+%     permidx=randperm(No_spect);
+%     trainY = Y(permidx(1:no_train),:);
+%     trainX = X(:,:,permidx(1:no_train));
+% 
+%     valX = X(:,:,permidx(no_train+1:end));
+%     valY = Y(permidx(no_train+1:end),:);
+% 
+%     x = permute(trainX, [3, 1, 2]);
+%     px = py.torch.tensor(py.numpy.array(x));
+%     px = px.cuda().float();
+%     opt.px = px; % added the myfunc to option
+% 
+%     opt.winsize = winsize;
+%     opt.px = px; % opt.px will be changed in validation
+%     trainNvec = N*ones(1,no_train);%N_vec(permidx(i,1:no_train));
+%     if opt.addone
+%         no_para=F*winsize+1;
+%     else
+%         no_para=F*winsize;
+%     end
+%     wini=1e-3*randn(no_para,C,K);
+%     [ w,~,loss,garr] = EMPosteriorRegularized_batch(...
+%         wini,trainX,trainY,trainNvec,EMiterations,Miterations,0,gamma,opt,lamb);
+% 
+%     % this part is newly added to see the signal level accuracy result
+%     [y_hat, p] = get_signal_label(w, valX, opt);  % opt.px will be changed
+%     acc = sum((y_hat - valY) == 0, 'all')/numel(y_hat)
+%     [rec, prec] = prec_rec(valY, y_hat)
+%     f1(r) = 2/(1/(rec+1e-30) + 1/(prec+1e-30))
+% 
+%     end % end of runs
+%     i
+%     mean_f1 = mean(f1)
+% end % end of i
+% 
+% % i = 6 is the best
+% % lamb, winzize, N, K
+% % 0.1, 100, 50, 2
+
 %% test
-load('/home/chenhao1/Matlab/data_matlab/ESC10/esc10_te.mat')
-X_test = permute(X, [2,3,1]);
-Y_test = Y;
+% load('/home/chenhao1/Matlab/data_matlab/ESC10/esc10_te.mat')
+% X_test = permute(X, [2,3,1]);
+% Y_test = Y;
+% load('/home/chenhao1/Matlab/data_matlab/ESC10/esc10_tr.mat')
+% X = permute(X, [2,3,1]);
+% 
+% runs = 5;
+% [lamb, winzize, N, K] = deal(0.1, 100, 50, 2); 
+% acc = zeros(runs, 1); 
+% f1 = zeros(runs, 1); 
+% opt.C = C;
+% opt.F = F;
+% opt.T = T;
+% opt.K = K;
+% 
+% for r=1:runs
+%     permidx=randperm(No_spect);
+%     trainY = Y(permidx(1:no_train),:);
+%     trainX = X(:,:,permidx(1:no_train));
+% 
+%     x = permute(trainX, [3, 1, 2]);
+%     px = py.torch.tensor(py.numpy.array(x));
+%     px = px.cuda().float();
+%     opt.px = px; % added the myfunc to option
+% 
+%     opt.winsize = winsize;
+%     opt.px = px; % opt.px will be changed in validation
+%     trainNvec = N*ones(1,no_train);%N_vec(permidx(i,1:no_train));
+%     if opt.addone
+%         no_para=F*winsize+1;
+%     else
+%         no_para=F*winsize;
+%     end
+%     wini=1e-3*randn(no_para,C,K);
+%     [ w,~,loss,garr] = EMPosteriorRegularized_batch(...
+%         wini,trainX,trainY,trainNvec,EMiterations,Miterations,0,gamma,opt,lamb);
+% 
+%     % this part is newly added to see the signal level accuracy result
+%     [y_hat, p] = get_signal_label(w, X_test, opt);  % opt.px will be changed
+%     acc = sum((y_hat - Y_test) == 0, 'all')/numel(y_hat)
+%     [rec, prec] = prec_rec(Y_test, y_hat)
+%     f1(r) = 2/(1/(rec+1e-30) + 1/(prec+1e-30))
+%     save('you_raich_yhat.mat', 'p')
+% end % end of runs
+% mean_f1 = mean(f1)
+
+
+
--- You_raich/get_signal_label.m
+++ You_raich/get_signal_label.m
@@ -1,4 +1,4 @@
-function y_hat = get_signal_label(W, X, option)
+function [y_hat, P] = get_signal_label(W, X, option)
 % this function is written to get the predicted labels in the signal level
 % the calculation process is in You's paper
 % basically it will get the union of the labels per window
@@ -6,16 +6,20 @@ function y_hat = get_signal_label(W, X, option)
 N = size(X, 3);  % number of samples
 C = size(W, 2);  % number of classes
 y_hat = zeros(N, C);
+P = zeros(N, C);
+thr = 0.5;
 for i = 1:N
     px = py.torch.tensor(py.numpy.array(X(:,:, i)));
     px = px.cuda().float().unsqueeze(py.int(0));
     option.px = px; % added the myfunc to option
     
-    [p,~]=PriorOneBag(W,X(:,:, i),option);  % p contains null as the last row shape [C+1, n_instance]
-    [~, ind] = max(p);
-    pool_label = 1:size(p, 1)-1;
-    label = intersect(pool_label, ind);
-    y_hat(i, label) = 1;
+    [pp,~]=PriorOneBag(W,X(:,:, i),option);  % pp contains null as the last row shape [C+1, n_instance]
+    [p, ~] = max(pp'); % p is per class probability
+    pool_label = p(1:10);
+    pool_label(pool_label>=thr) = 1;
+    pool_label(pool_label<thr) = 0;
+    y_hat(i, :) = pool_label;
+    P(i, :) = p(1:10);
 end
 
 end  % end of the file
