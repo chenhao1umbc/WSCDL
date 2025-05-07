c878b08a9a0386b43f44429896b79844eda4b265
chenhao1umbc <chenhao1@umbc.edu>
2025-05-07 02:23:57 +0000

    merged yusen changes

--- main_toy.py
+++ main_toy.py
@@ -4,14 +4,13 @@ The default data type is torch.tensor with precision float32
 
 # %%
 from utils import *
-
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 opts = OPT()
 opts.snr = 10
 opts.lamb = 3  # for sparsity penalty
 opts.eta = 3  # for label penalty
 opts.mu = 0.1  # for low rank penalty
-opts.savefig = False  # save plots
+opts.savefig = False # save plots
 opts.show_details = False  # default as true
 
 # %% training section
@@ -21,8 +20,8 @@ D, D0, S, S0, W = init(X, opts)
 D, D0, S, S0, W, loss = train(D, D0, S, S0, W, X, Y, opts)
 plot_result(X, Y, D, D0, S, S0, W, opts.ft, loss, opts)
 
-# %% testing section
-X_test, Y_test, *_ = load_toy(opts, test="cv")
+#%% testing section
+X_test, Y_test, *_ = load_toy(opts, test='cv')
 _, _, S_t, S0_t, _ = init(X_test, opts)
 acc, y_hat, S_t, S0_t, loss_t = test(D, D0, S_t, S0_t, W, X_test, Y_test, opts)
 print("\nThe test data accuracy is : ", acc.acc)
